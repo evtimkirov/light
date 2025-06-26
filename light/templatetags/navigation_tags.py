abe9789f6f607ca7d309a4471c1455251cfe5445
Evtim Kirov <evtimkirov@gmail.com>
2025-06-26 13:26:33 +0300

    Revert "Push the entire project"
    This reverts commit e81d2b01cb80b8e0d9309f08ae31c42ba1148f5e.

--- light/templatetags/navigation_tags.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag
-def is_active(request, path):
-    return 'active' if request.path == path else ''
--- light/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import homepage
-
-urlpatterns = [
-    path('', homepage, name='homepage'),
-    path('', include('apps.terminals.urls')),
-    path('admin/', admin.site.urls),
-]
